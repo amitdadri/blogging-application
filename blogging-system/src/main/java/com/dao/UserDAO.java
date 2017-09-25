4694df46b910cb044f018132bb3d84f4f8deea65
amitdadri <32186651+amitdadri@users.noreply.github.com>
2017-09-25 19:27:18 +0530

    Add files via upload

--- blogging-system/src/main/java/com/dao/UserDAO.java
+++ blogging-system/src/main/java/com/dao/UserDAO.java
@@ -1,10 +1,21 @@
-package com.BloggingApplication.DAO;
-import com.BloggingApplication.model.*;
+package com.blog.uu;
 
+import java.sql.Timestamp;
+import java.util.List;
 
 public interface UserDAO {
-	
 	public void createUser(User user);
-	public boolean findUser(String username,String password);
-
+	public User findUser(String username);
+	public void removeUser(User user);
+	public List<User>getUsers();
+	public void update(User user);
+	public void removeUser(String username);
+	public int login(String username,String password);
+	public Blog viewblog(int blogid);
+	public Blog searchblog(String category);
+	public void createblog(Blog blog);
+	public void deleteblog(int blog);
+	public void updateblog(int bid,String title,Timestamp time,String des);	
+	public void updateblog1(int bid,Blog blog);	
+	public void postcomment(Comment comment);
 }
