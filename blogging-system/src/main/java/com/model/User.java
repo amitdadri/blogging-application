05499399517a158af4f9de40eb990624e0455a11
amitdadri <32186651+amitdadri@users.noreply.github.com>
2017-09-25 19:30:02 +0530

    Add files via upload

---
+++ blogging-system/src/main/java/com/model/User.java
@@ -0,0 +1,114 @@
+package com.blog.uu;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the USERS database table.
+ * 
+ */
+@Entity
+@Table(name="USERS")
+//@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String username;
+
+	private String emailid;
+
+	private String password;
+
+	private String usertype;
+
+	//bi-directional many-to-one association to Comment
+	@OneToMany(mappedBy="user")
+	private List<Comment> comments;
+
+//	//bi-directional many-to-one association to Rating
+//	@OneToMany(mappedBy="user")
+//	private List<Rating> ratings;
+//
+	public User() {
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmailid() {
+		return this.emailid;
+	}
+
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsertype() {
+		return this.usertype;
+	}
+
+	public void setUsertype(String usertype) {
+		this.usertype = usertype;
+	}
+
+	public List<Comment> getComments() {
+		return this.comments;
+	}
+
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
+
+	public Comment addComment(Comment comment) {
+		getComments().add(comment);
+		comment.setUser(this);
+
+		return comment;
+	}
+
+	public Comment removeComment(Comment comment) {
+		getComments().remove(comment);
+		comment.setUser(null);
+
+		return comment;
+	}
+//
+//	public List<Rating> getRatings() {
+//		return this.ratings;
+//	}
+//
+//	public void setRatings(List<Rating> ratings) {
+//		this.ratings = ratings;
+//	}
+//
+//	public Rating addRating(Rating rating) {
+//		getRatings().add(rating);
+//		rating.setUser(this);
+//
+//		return rating;
+//	}
+//
+//	public Rating removeRating(Rating rating) {
+//		getRatings().remove(rating);
+//		rating.setUser(null);
+//
+//		return rating;
+//	}
+
+}
